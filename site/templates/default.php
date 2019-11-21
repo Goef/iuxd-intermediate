200466823fe1718443965865a70299f3c90fe76b
Goef <geoffrey0807@hotmail.com>
2019-11-21 22:10:17 +0100

    Start :)

--- site/templates/default.php
+++ site/templates/default.php
@@ -9,11 +9,90 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     <title><?= $page->title() ?></title>
+    <style>
+      body {
+
+      }
+
+      .containerG{
+        display:flex;
+        justify-content:center;
+
+      }
+  
+      .poes1{
+        width: 60vw;
+        height: 80vh;
+        padding-bottom:40px;
+      }
+      .poes2{
+        width: 60vw;
+        height: 60vh;
+      }
+      .poes4{
+        width: 10vw;
+        height: 10vh;
+      }
+      .fourCom{
+        padding-bottom: 60px;
+      }
+   </style>
   </head>
   <body>
-    <h1><?= $page->title() ?></h1>
+    <div class="containerG">
+      <div class="wireframing">
+<p><b>Wireframing</b></p>    
+<p>Hoe gebruik je ze en waar zijn ze nou eigenlijk goed voor</p>
+<img src="https://www.zooplus.nl/magazine/wp-content/uploads/2018/08/arriv%C3%A9e-dun-chaton-%C3%A0-la-maison.jpeg" alt="poes" class="poes1">
+</div>
+    </div>
+    <div class="containerG">
+      <div class="wireframing">
+<p>
+Je hebt je uiteindelijke ontwerp bij de opdrachtgever opgeleverd om er uiteindelijk achter te komen dat het werk <br> 
+ dat je hebt gemaakt ver van het doel was; je bent weer terug bij af. De uren die u aan het oorspronkelijke ontwerp hebt besteed,<br>
+  zijn verloren gegaan. Dit en andere scenario's zijn precies de reden waarom wireframes een essentiële stap zijn om van<br>
+   een digitaal product een realiteit te maken. Denk aan een wireframe zoals een blauwdruk van een gebouw, maar dan voor <br>
+    een digitaal product.
+</p>
+<img src="https://www.zooplus.nl/magazine/wp-content/uploads/2018/08/arriv%C3%A9e-dun-chaton-%C3%A0-la-maison.jpeg" alt="poes" class="poes2">
+</div>
+    </div>
 
-    <p><?= $page->text()->kirbytext() ?></p>
+    <div class="container fourCom">
+      <p> <b> Met wireframes kan je vier dingen doorcommuniceren:</b></p>
+    <div class="row">
+    <div class="col-sm">
+<p>Structuur</p>   
+   <img src="https://www.zooplus.nl/magazine/wp-content/uploads/2018/08/arriv%C3%A9e-dun-chaton-%C3%A0-la-maison.jpeg" alt="poes" class="poes4">
+
+    </div>
+    <div class="col-sm">
+    <p>Layout</p>   
+      <img src="https://www.zooplus.nl/magazine/wp-content/uploads/2018/08/arriv%C3%A9e-dun-chaton-%C3%A0-la-maison.jpeg" alt="poes" class="poes4">
+
+    </div>
+    <div class="col-sm">
+    <p>Content</p>   
+      <img src="https://www.zooplus.nl/magazine/wp-content/uploads/2018/08/arriv%C3%A9e-dun-chaton-%C3%A0-la-maison.jpeg" alt="poes" class="poes4">
+
+    </div>
+    <div class="col-sm">
+    <p>Functie</p>   
+      <img src="https://www.zooplus.nl/magazine/wp-content/uploads/2018/08/arriv%C3%A9e-dun-chaton-%C3%A0-la-maison.jpeg" alt="poes" class="poes4">
+
+    </div>
+  </div>
+    </div>
+    <div class="containerG">
+      <div class="wireframing">
+<p><b>Maar waar past wireframing in het productontwerpproces</b></p>    
+<p>Om wireframes te kunnen maken heb je ontwerprichtlijnen nodig. Ontwerprichtlijnen zijn korte en krachtige statements <br>
+   (Onderbouwd aan de hand van onderzoeksresultaten), waarin de essentie van de ervaring die een gebruiker met je product <br>
+    of dienst heeft wordt beschreven. Ze geven richting aan het ontwerpproces en bieden houvast bij het nemen van <br>
+     beslissingen. (Hinfelaar, 2014)</p>
+<img src="https://www.zooplus.nl/magazine/wp-content/uploads/2018/08/arriv%C3%A9e-dun-chaton-%C3%A0-la-maison.jpeg" alt="poes" class="poes1">
+</div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
